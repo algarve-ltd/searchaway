6eed97203885a697f720789b4edd0db3d4f345d7
Brad <brad@128bitstudios.co.uk>
2025-10-23 23:48:24 +0100

    refactor: remove unused useRef import from DestinationModal

--- apps/client-server/src/components/common/DestinationModal.tsx
+++ apps/client-server/src/components/common/DestinationModal.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import destinationData from "@/data/DestinationData.json";
 
